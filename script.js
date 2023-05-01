f6477895e2cbd87b54805fec4fa6b5702d4a736c
vladprogrim <113541340+vladprogrim@users.noreply.github.com>
2023-05-01 23:33:01 +0300

    docs: virtual-keyboard2

--- script.js
+++ script.js
@@ -1,54 +1,127 @@
-// Создаем элемент-контейнер для клавиатуры
-const keyboardContainer = document.createElement('div');
-keyboardContainer.style.display = 'flex';
-keyboardContainer.style.flexWrap = 'wrap';
-keyboardContainer.style.width = '400px';
-keyboardContainer.style.margin = 'auto';
-document.body.appendChild(keyboardContainer);
+// Создаем элементы клавиатуры
+const keyboardElement = document.createElement("div");
+keyboardElement.classList.add("keyboard");
 
-// Создаем элемент-контейнер для отображения нажатых клавиш
-const screenContainer = document.createElement('div');
-screenContainer.style.width = '400px';
-screenContainer.style.height = '50px';
-screenContainer.style.border = '1px solid black';
-screenContainer.style.margin = '10px auto';
-screenContainer.style.textAlign = 'center';
-screenContainer.style.lineHeight = '50px';
-keyboardContainer.parentNode.insertBefore(screenContainer, keyboardContainer.nextSibling);
+const keysContainer = document.createElement("div");
+keysContainer.classList.add("keys");
 
-// Создаем массив с символами клавиатуры
-const keyboardKeys = [
-    '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=',
-    'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\',
-    'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter',
-    'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'Shift', 'Fn', 'Ctrl', 'Alt', 'Space', 'AltGr', 'Ctrl', '←', '↑', '↓', '→',
-    'Backspace'
-  ];
+keyboardElement.appendChild(keysContainer);
+document.body.appendChild(keyboardElement);
 
-  let inputString = '';
+// Определяем раскладку для русской и английской клавиатур
+const RUSSIAN_LAYOUT = [
+  "ё 1 2 3 4 5 6 7 8 9 0 - = {bksp}",
+  "{tab} й ц у к е н г ш щ з х ъ \\",
+  "{lock} ф ы в а п р о л д ж э {enter}",
+  "{shift} я ч с м и т ь б ю . {shift}",
+  ".com {space}"
+];
 
-  // Добавляем символы клавиатуры на экран
-  keyboardKeys.forEach((key) => {
-    const keyElement = document.createElement('button');
-    keyElement.textContent = key;
-    keyElement.style.width = '30px';
-    keyElement.style.height = '30px';
-    keyElement.style.margin = '5px';
-    keyElement.addEventListener('click', () => {
-      if (key === 'Backspace') {
-        inputString = inputString.slice(0, -1);
-      } else {
-        inputString += key;
+const ENGLISH_LAYOUT = [
+  "` 1 2 3 4 5 6 7 8 9 0 - = {bksp}",
+  "{tab} q w e r t y u i o p [ ] \\",
+  "{lock} a s d f g h j k l ; ' {enter}",
+  "{shift} z x c v b n m , . / {shift}",
+  ".com {space}"
+];
+
+// Определяем текущую раскладку и создаем клавиши
+let currentLayout = RUSSIAN_LAYOUT;
+
+function createKeys(layout) {
+  layout.forEach(keyRow => {
+    const keyRowElement = document.createElement("div");
+    keyRowElement.classList.add("keyrow");
+
+    keyRow.split(" ").forEach(key => {
+      const keyElement = document.createElement("button");
+      keyElement.classList.add("key");
+
+      switch (key) {
+        case "{bksp}":
+          keyElement.textContent = "Backspace";
+          keyElement.addEventListener("click", () => {
+            textareaElement.value = textareaElement.value.substring(
+              0,
+              textareaElement.value.length - 1
+            );
+          });
+          break;
+
+        case "{tab}":
+          keyElement.textContent = "Tab";
+          keyElement.addEventListener("click", () => {
+            textareaElement.value += "\t";
+          });
+          break;
+
+        case "{lock}":
+          keyElement.textContent = "Caps Lock";
+          break;
+
+        case "{enter}":
+          keyElement.textContent = "Enter";
+          keyElement.addEventListener("click", () => {
+            textareaElement.value += "\n";
+          });
+          break;
+
+        case "{shift}":
+          keyElement.textContent = "Shift";
+          break;
+
+        case ".com":
+          keyElement.textContent = ".com";
+          break;
+
+        default:
+          keyElement.textContent = key.toLowerCase();
+          keyElement.addEventListener("click", () => {
+            textareaElement.value += keyElement.textContent;
+          });
+          break;
       }
-      screenContainer.textContent = inputString;
+
+      keyRowElement.appendChild(keyElement);
     });
-    keyboardContainer.appendChild(keyElement);
+
+    keysContainer.appendChild(keyRowElement);
+  });
+}
+
+createKeys(currentLayout);
+
+// Добавляем текстовое поле
+const textareaElement = document.createElement("textarea");
+document.body.appendChild(textareaElement);
+
+// Переключение на английскую раскладку при нажатии
+document.addEventListener("keydown", event => {
+    const key = event.key;
+
+    if (key === "Shift") {
+      currentLayout = ENGLISH_LAYOUT;
+      keysContainer.innerHTML = "";
+      createKeys(currentLayout);
+    }
+
+    if (key === "Alt") {
+      altPressed = true;
+    }
   });
 
-  // Добавляем обработчик событий для клавиши backspace на клавиатуре
-  document.addEventListener('keydown', (event) => {
-    if (event.key === 'Backspace') {
-      inputString = inputString.slice(0, -1);
-      screenContainer.textContent = inputString;
+  document.addEventListener("keyup", event => {
+    const key = event.key;
+
+    if (key === "Shift") {
+      if (!altPressed) {
+        currentLayout = RUSSIAN_LAYOUT;
+        keysContainer.innerHTML = "";
+        createKeys(currentLayout);
+      }
+    }
+
+    if (key === "Alt") {
+      altPressed = false;
     }
   });
